6573f14e644fc82dec4570b2b0f7c486a6e81114
Alexander Rodriguez <alexander.personal24112003@gmail.com>
2025-05-19 19:52:04 -0400

    feat: restructure authentication components and update routes

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -1,26 +1,26 @@
-import{ useState } from 'react';
-import { useAuth } from '../context/AuthContext';
+import  { useState } from 'react';
+import { useAuth } from '../../context/AuthContext';
 
-export default function Login() {
-  const { login } = useAuth();
+export default function Signup() {
+  const { signup } = useAuth();
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
   const handleSubmit = async (e: { preventDefault: () => void; }) => {
     e.preventDefault();
-    const error = await login(email, password);
+    const error = await signup(email, password);
     if (error) {
       setErrorMsg(error.message);
     } else {
       setErrorMsg(null);
-   
+ 
     }
   };
 
   return (
     <form onSubmit={handleSubmit} className="max-w-sm mx-auto p-4">
-      <h2 className="text-2xl mb-4">Login</h2>
+      <h2 className="text-2xl mb-4">Registro</h2>
       {errorMsg && <p className="text-red-500">{errorMsg}</p>}
       <input
         type="email"
@@ -38,7 +38,7 @@ export default function Login() {
         onChange={(e) => setPassword(e.target.value)}
         required
       />
-      <button type="submit" className="bg-blue-600 text-white py-2 w-full">Entrar</button>
+      <button type="submit" className="bg-green-600 text-white py-2 w-full">Registrarse</button>
     </form>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './components/Login'
+import Login from './components/auth/Login'
 import { AuthProvider } from './context/AuthContext'
+import Signup from './components/auth/Signup'
 
 
 
@@ -16,7 +17,8 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='*' element={<h1 className='text-6xl text-red-600 flex justify-center items-center'>404</h1>} />
-        <Route path='/login' element={<Login/>}/>
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup/>} />
      
       </Routes>
     </BrowserRouter>
