114686a8d9f002fc42ddcc5b2788e0b6b69ffd20
mrxandev <alexander.personal24112003@gmail.com>
2025-06-20 10:16:22 -0400

    Agregar Login y Signup al navbar

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import Login from "../components/auth/Login"
+
+
+function LoginPage() {
+  return (
+   <>
+   <Login/>
+   </>
+  )
+}
+
+export default LoginPage
---
+++ src/pages/SignupPage.tsx
@@ -0,0 +1,12 @@
+import Signup from "../components/auth/Signup"
+
+
+function SignupPage() {
+  return (
+   <>
+   <Signup/>
+   </>
+  )
+}
+
+export default SignupPage
