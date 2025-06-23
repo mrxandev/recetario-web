80b9c78fc907b3d1acaad92369e09df1bae972cd
mrxandev <alexander.personal24112003@gmail.com>
2025-06-23 10:42:02 -0400

    Login & Logout

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,8 +1,10 @@
 import{ useState } from 'react';
 import { useAuth } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
   const { login } = useAuth();
+  const navigate=useNavigate()
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
@@ -10,15 +12,19 @@ export default function Login() {
   const handleSubmit = async (e: { preventDefault: () => void; }) => {
     e.preventDefault();
     const error = await login(email, password);
+  
     if (error) {
       setErrorMsg(error.message);
     } else {
       setErrorMsg(null);
+      navigate("/")
    
     }
   };
 
   return (
+    <>
+    <div className="w-[90%] h-screen flex justify-center items-center">
     <form onSubmit={handleSubmit} className="max-w-sm mx-auto p-4">
       <h2 className="text-2xl mb-4">Login</h2>
       {errorMsg && <p className="text-red-500">{errorMsg}</p>}
@@ -38,7 +44,9 @@ export default function Login() {
         onChange={(e) => setPassword(e.target.value)}
         required
       />
-      <button type="submit" className="bg-blue-600 text-white py-2 w-full">Entrar</button>
+      <button type="submit" className="bg-blue-600 text-white py-2 w-full">Acceder</button>
     </form>
+    </div>
+    </>
   );
 }
